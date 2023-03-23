133210fbe42c2a6ceeb5dd9edf083e0454225746
Patricia <wambuipatricia3@gmail.com>
2023-03-23 13:32:02 +0300

    Completed Assignments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    const taskForm = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    taskForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const taskInput = document.getElementById("new-task-description");
+      const taskText = taskInput.value;
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+      taskList.appendChild(taskItem);
+      taskInput.value = "";
+    });
+  });
+  
